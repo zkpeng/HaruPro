e2b79541f6aa2bde84f32850edc984a312bf70b4
zhangkepeng <1015717288@qq.com>
2018-10-08 16:20:54 +0800

    增加判断PDF文件是否生成成功的代码

--- app/src/main/java/com/zkp/inc/hpdfpro/MainActivity.java
+++ app/src/main/java/com/zkp/inc/hpdfpro/MainActivity.java
@@ -3,8 +3,8 @@ package com.zkp.inc.hpdfpro;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +30,12 @@ public class MainActivity extends AppCompatActivity {
      * @param view
      */
     public void generate(View view) {
-        generatePdf(etText.getText().toString(), Integer.parseInt(etSize.getText().toString()), Float.parseFloat(etSpace.getText().toString()));
+        int retCode = generatePdf(etText.getText().toString(), Integer.parseInt(etSize.getText().toString()), Float.parseFloat(etSpace.getText().toString()));
+        if (retCode == 0) {
+            Toast.makeText(this, "生成PDF文件成功,\n位置/sdcard/test.pdf", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "生成PDF文件失败", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public native int generatePdf(String text, int txtSize, float space);
